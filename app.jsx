b440a3b4421136f9d10d438740f6c8809c7b132b
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-02-09 18:09:15 -0800

    Adding react framework for building UI

---
+++ app.jsx
@@ -0,0 +1,8 @@
+var React = require("react");
+document.addEventListener(
+    "DOMContentLoaded",
+    function(event) {
+        React.render(<h1>hello</h1>, document.getElementById('content'));
+    }
+)
+
